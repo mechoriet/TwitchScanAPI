af00ba7ba4a166ff0ef9bfb3231533980d84b147
LenBanana <lenstandvoss@yahoo.de>
2024-09-29 08:39:57 +0200

    Make the time period for activity also be in 5 minutes and return more data

--- TwitchScanAPI/Data/Statistics/Chat/PeakActivityPeriodStatistic.cs
+++ TwitchScanAPI/Data/Statistics/Chat/PeakActivityPeriodStatistic.cs
@@ -17,7 +17,7 @@ namespace TwitchScanAPI.Data.Statistics.Chat
             // Return the top 3 hours with the highest message counts
             return _hourlyMessageCounts
                 .OrderByDescending(kv => kv.Value)
-                .Take(10)
+                .Take(100)
                 .ToDictionary(kv => kv.Key, kv => kv.Value);
         }
 
@@ -25,16 +25,18 @@ namespace TwitchScanAPI.Data.Statistics.Chat
         {
             if (message?.Time == null) return; // Handle null message or time
 
-            // Get the hour in UTC format as MM/dd/yyyy-HH:00:00
+            // Get the time in UTC format
             var dateTime = message.Time.ToUniversalTime();
-            // Round the minutes to the nearest 10
-            var roundedMinutes = Math.Floor((double)dateTime.Minute / 10) * 10;
-            var hour = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, (int)roundedMinutes, 0)
+    
+            // Round the minutes to the nearest 5-minute period
+            var roundedMinutes = Math.Floor((double)dateTime.Minute / 5) * 5;
+
+            // Create a new DateTime with the rounded minutes
+            var roundedTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, (int)roundedMinutes, 0)
                 .ToString("yyyy-MM-ddTHH:mm:ss");
 
-
-            // Add or update the count for the current hour in a thread-safe manner
-            _hourlyMessageCounts.AddOrUpdate(hour, 1, (key, oldValue) => oldValue + 1);
+            // Add or update the count for the current rounded time in a thread-safe manner
+            _hourlyMessageCounts.AddOrUpdate(roundedTime, 1, (key, oldValue) => oldValue + 1);
         }
     }
 
