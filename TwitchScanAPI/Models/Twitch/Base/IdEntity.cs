116f9a624c03a98d5b863f5a32ab657492dee00a
LenBanana <lenstandvoss@yahoo.de>
2025-02-16 17:44:03 +0100

    Do not send id to clients

--- TwitchScanAPI/Models/Twitch/Base/IdEntity.cs
+++ TwitchScanAPI/Models/Twitch/Base/IdEntity.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace TwitchScanAPI.Models.Twitch.Base
 {
     public class IdEntity
     {
+        [JsonIgnore]
         public Guid Id { get; set; } = Guid.NewGuid();
     }
 }
